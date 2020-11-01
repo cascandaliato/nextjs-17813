b02c3c6faa63a641814f8bdc9e6313a0047a374f
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-11-01 20:29:40 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+export default function Home() {
+  return (
+    <div>
+      <Link as="/world" href="/[name]">
+        <a>Hello world</a>
+      </Link>
+    </div>
+  )
+}
